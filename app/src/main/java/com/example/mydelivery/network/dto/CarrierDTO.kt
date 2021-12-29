cfc768c3d4720e5279a3e5d3daaada1ecbcd0688
LeeJaeWon <zhtm2@openobject.net>
2021-12-29 13:24:18 +0900

    - CarrierDTO.kt converted to Data Class - title in toolbar will implement align to center

--- app/src/main/java/com/example/mydelivery/network/dto/CarrierDTO.kt
+++ app/src/main/java/com/example/mydelivery/network/dto/CarrierDTO.kt
@@ -1,14 +1,17 @@
 package com.example.mydelivery.network.dto
+
 import com.google.gson.annotations.SerializedName
-class CarrierDTO {
+
+data class CarrierDTO(
     @SerializedName("id")
-    var id : String = ""
+    var id: String = "",
 
     @SerializedName("name")
-    var name : String = ""
+    var name: String = "",
 
     @SerializedName("tel")
-    var tel : String = ""
+    var tel: String = ""
+) {
     override fun toString(): String {
         return "CarriersDTO(id='$id', name='$name', tel='$tel')"
     }
